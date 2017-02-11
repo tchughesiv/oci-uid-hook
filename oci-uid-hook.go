1d138ee9a4ebee98052a2adabf225e36688fa3de
Tommy Hughes <tchughesiv@gmail.com>
2017-02-11 11:11:37 -0600

    log, err, & order cleanup

--- oci-uid-hook.go
+++ oci-uid-hook.go
@@ -25,37 +25,38 @@ import (
 	"gopkg.in/yaml.v1"
 )
 
-// CONFIG uid hook configuration
 const (
-	CONFIG           = "/etc/oci-uid-hook.conf" // Config file for disabling hook
+	config           = "/etc/oci-uid-hook.conf" // Config file for disabling hook
 	dockerAPIversion = "1.24"                   // docker server api version
 	pfile            = "/etc/passwd"            // passwd path in container
 	ctxTimeout       = 10 * time.Second         // docker client timeout
 )
 
-var spec specs.Spec
-var state specs.State
-var containerJSON ContainerJSON
-var settings struct {
-	Disabled bool `yaml:"disabled"`
+var (
+	spec          specs.Spec
+	state         specs.State
+	containerJSON ContainerJSON
+	check         string
+	username      string
+	usercheck     bool
+	//usergid string
+
+	settings struct {
+		Disabled bool `yaml:"disabled"`
+	}
+)
+
+// ContainerJSON is newly used struct along with MountPoint
+type ContainerJSON struct {
+	*types.ContainerJSONBase
+	MountPoints MountPointData
+	Config      *container.Config
 }
 
-//	configFileName               = "config.v2.json"
-// https://github.com/docker/docker/blob/v1.12.5/libcontainerd/client.go
-// https://github.com/docker/docker/blob/v1.12.5/libcontainerd/client_linux.go
-// https://github.com/docker/docker/blob/v1.12.5/libcontainerd/container.go
-// https://github.com/docker/docker/tree/v1.12.5/libcontainerd
-//func (ctr *container) spec() (*specs.Spec, error) {
-//	var spec specs.Spec
-//	dt, err := ioutil.ReadFile(filepath.Join(ctr.dir, configFilename))
-//	if err != nil {
-//		return nil, err
-//	}
-//	if err := json.Unmarshal(dt, &spec); err != nil {
-//		return nil, err
-//	}
-//	return &spec, nil
-//}
+// MountPointData represents a mount point configuration inside the container.
+type MountPointData struct {
+	MountPoint types.MountPoint `json:"/etc/passwd"`
+}
 
 func main() {
 	os.Setenv("DOCKER_API_VERSION", dockerAPIversion)
@@ -66,52 +67,40 @@ func main() {
 	}
 
 	// config file settings
-	data, err := ioutil.ReadFile(CONFIG)
+	data, err := ioutil.ReadFile(config)
 	if err != nil {
 		if !os.IsNotExist(err) {
-			log.Fatalf("UIDHook Failed to read %s %v", CONFIG, err.Error())
+			log.Printf("UIDHook Failed to read %s %v", config, err.Error())
 		}
 	} else {
 		if err := yaml.Unmarshal(data, &settings); err != nil {
-			log.Fatalf("UIDHook Failed to parse %s %v", CONFIG, err.Error())
+			log.Printf("UIDHook Failed to parse %s %v", config, err.Error())
 		}
 		if settings.Disabled {
 			return
 		}
 	}
 
-	if err := json.NewDecoder(os.Stdin).Decode(&state); err != nil {
-		log.Fatalf("UIDHook Failed %v", err.Error())
-	}
-
-	// newconfigFile := fmt.Sprintf("%s/config.json", state.BundlePath)
-	// procPasswd := fmt.Sprintf("/proc/%d/root/etc/passwd", state.Pid)
-	configFile := os.Args[2]
 	command := os.Args[1]
+	configFile := os.Args[2]
 	cpath := path.Dir(configFile)
-	newPasswd := fmt.Sprintf("%s/passwd", cpath)
+
+	if err := json.NewDecoder(os.Stdin).Decode(&state); err != nil {
+		log.Printf("UIDHook Failed %v", err.Error())
+	}
+	// newconfigFile := fmt.Sprintf("%s/config.json", state.BundlePath)
 
 	// get additional container info
 	jsonFile, err := ioutil.ReadFile(configFile)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 	json.Unmarshal(jsonFile, &containerJSON)
-	ugidresult := strings.Split(containerJSON.Config.User, ":")
-	user := ugidresult[0]
-	pwMount := containerJSON.MountPoints.MountPoint.Destination
 
 	switch command {
 	case "prestart":
 		{
-			// check for existing /etc/passwd bind mount... bypass if exists
-			if pwMount != "" {
-				log.Printf("Hook bypassed: /etc/passwd already bind mounted")
-				return
-			}
-
-			log.Printf("%s %s", command, state.ID)
-			if err = UIDHook(containerJSON.Config.Image, state.ID, user, cpath, newPasswd); err != nil {
+			if err = UIDHook(command, containerJSON.Config.Image, state.ID, cpath); err != nil {
 			}
 			return
 		}
@@ -120,11 +109,11 @@ func main() {
 			return
 		}
 	}
-	log.Fatalf("Invalid command %q must be prestart|poststop", command)
+	log.Printf("Invalid command %q must be prestart|poststop", command)
 }
 
 // UIDHook for username recognition w/ arbitrary uid in the container
-func UIDHook(image string, id string, user string, cpath string, newPasswd string) error {
+func UIDHook(command string, image string, id string, cpath string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), ctxTimeout)
 	defer cancel()
 	cli, _ := client.NewEnvClient()
@@ -132,10 +121,12 @@ func UIDHook(image string, id string, user string, cpath string, newPasswd strin
 	// retrieve image user
 	imageJSON, imageOUT, err := cli.ImageInspectWithRaw(ctx, image)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 	_ = imageOUT
 	imageUser := imageJSON.Config.User
+	ugidresult := strings.Split(containerJSON.Config.User, ":")
+	user := ugidresult[0]
 
 	// check if container user matches image user
 	if eq := strings.Compare(imageUser, user); eq == 0 {
@@ -146,20 +137,27 @@ func UIDHook(image string, id string, user string, cpath string, newPasswd strin
 	if _, err := strconv.Atoi(user); err != nil {
 		return nil
 	}
-	useruid := user
+
+	log.Printf("%s %s", command, state.ID)
+
+	// check for existing /etc/passwd bind mount... bypass if exists
+	pwMount := containerJSON.MountPoints.MountPoint.Destination
+	if pwMount != "" {
+		log.Printf("hook bypassed: %s already mounted", pfile)
+		return nil
+	}
 
 	// retrieve passwd file from container
+	newPasswd := fmt.Sprintf("%s/passwd", cpath)
+	// procPasswd := fmt.Sprintf("/proc/%d/root/etc/passwd", state.Pid)
 	imageName := imageJSON.ID
 	fileRetrieve(imageName, newPasswd, cpath)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 
-	var username string
-	var usercheck bool
-	// var usergid string
-
 	pwFile, err := os.Open(newPasswd)
+	useruid := user
 	in, err := ioutil.ReadAll(pwFile)
 	lines := strings.Split(string(in), "\n")
 	for i, line := range lines {
@@ -172,15 +170,15 @@ func UIDHook(image string, id string, user string, cpath string, newPasswd strin
 			usercheck = true
 		}
 	}
+
 	findS := fmt.Sprintf("%s:x:%s:", username, imageUser)
 	replaceS := fmt.Sprintf("%s:x:%s:", username, useruid)
 
-	// ensure specified uid doesn't already match image username
+	// ensure specified uid doesn't already match an image username
 	if username != "" {
 		if usercheck != true {
-			log.Printf("hook engaged: %s", newPasswd)
-			replace(findS, replaceS, lines, newPasswd)
-			mountConfig(id, username, imageUser, useruid, newPasswd)
+			uidReplace(findS, replaceS, lines, newPasswd)
+			mountPasswd(id, username, imageUser, useruid, newPasswd)
 		}
 	}
 	return err
@@ -199,16 +197,16 @@ func fileRetrieve(imageName string, newPasswd string, cpath string) error {
 	}
 	tcuid, err := cli.ContainerCreate(ctx, containertmpConfig, nil, nil, "")
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 	cfile, stat, err := cli.CopyFromContainer(ctx, tcuid.ID, pfile)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 	_ = stat
 	c, err := ioutil.ReadAll(cfile)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 	cfile.Close()
 	crm := cli.ContainerRemove(ctx, tcuid.ID, types.ContainerRemoveOptions{
@@ -216,65 +214,27 @@ func fileRetrieve(imageName string, newPasswd string, cpath string) error {
 		Force: true,
 	})
 	if crm != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 
 	// create copy of passwd file in cpath
 	err = ioutil.WriteFile(newPasswd+".tar", c, 0644)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 	err = untar(newPasswd+".tar", cpath)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 	err = os.Remove(newPasswd + ".tar")
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 
 	return nil
 }
 
-func replace(findS string, replaceS string, lines []string, newPasswd string) {
-	// find/replace w/ new uid
-	var check string
-	for i, line := range lines {
-		if strings.Contains(line, findS) {
-			lines[i] = strings.Replace(lines[i], findS, replaceS, -1)
-			check = lines[i]
-		}
-	}
-	output := strings.Join(lines, "\n")
-	err := ioutil.WriteFile(newPasswd, []byte(output), 0644)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	log.Printf("passwd entry replaced w/: '%s'", check)
-	return
-}
-
-func mountConfig(id string, username string, imageUser string, useruid string, newPasswd string) {
-	// modify the jsonFile2 directly... add /etc/passwd bind mount
-	//  containerNewMount := &containerJSON.MountPoints{
-	//		"/etc/passwd": {
-	//			"Source":      newPasswd,
-	//			"Destination": "/etc/passwd",
-	//			"RW":          true,
-	//			"Name":        "",
-	//			"Driver":      "",
-	//			"Relabel":     "Z",
-	//			"Propagation": "rprivate",
-	//			"Named":       false,
-	//			"ID":          "",
-	//		},
-	//	}
-
-	log.Printf("passwd file mount complete")
-	return
-}
-
+// untar a tarball to a location
 func untar(tarball, target string) error {
 	reader, err := os.Open(tarball)
 	if err != nil {
@@ -313,15 +273,58 @@ func untar(tarball, target string) error {
 	return nil
 }
 
-// ContainerJSON is newly used struct along with MountPoint
-type ContainerJSON struct {
-	*types.ContainerJSONBase
-	MountPoints MountPointData
-	Config      *container.Config
+// uidReplace replaces image uid w/ specified uid in new passwd file
+func uidReplace(findS string, replaceS string, lines []string, newPasswd string) {
+	// find/replace w/ new uid
+	for i, line := range lines {
+		if strings.Contains(line, findS) {
+			lines[i] = strings.Replace(lines[i], findS, replaceS, -1)
+			check = lines[i]
+		}
+	}
+	output := strings.Join(lines, "\n")
+	err := ioutil.WriteFile(newPasswd, []byte(output), 0644)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Printf("passwd entry replaced w/ '%s' @ %s", check, newPasswd)
+	return
 }
 
-// MountPointData represents a mount point configuration inside the container.
-// This is used for reporting the mountpoints in use by a container.
-type MountPointData struct {
-	MountPoint types.MountPoint `json:"/etc/passwd"`
+// mountPasswd bind mounts new passwd into container
+func mountPasswd(id string, username string, imageUser string, useruid string, newPasswd string) {
+	// modify the jsonFile2 directly... add /etc/passwd bind mount
+	//  containerNewMount := &containerJSON.MountPoints{
+	//		"/etc/passwd": {
+	//			"Source":      newPasswd,
+	//			"Destination": "/etc/passwd",
+	//			"RW":          true,
+	//			"Name":        "",
+	//			"Driver":      "",
+	//			"Relabel":     "Z",
+	//			"Propagation": "rprivate",
+	//			"Named":       	lse,
+	//			"ID":          "",
+	//		},
+	//	}
+
+	//	configFileName               = "config.v2.json"
+	// https://github.com/docker/docker/blob/v1.12.5/libcontainerd/client.go
+	// https://github.com/docker/docker/blob/v1.12.5/libcontainerd/client_linux.go
+	// https://github.com/docker/docker/blob/v1.12.5/libcontainerd/container.go
+	// https://github.com/docker/docker/tree/v1.12.5/libcontainerd
+	//func (ctr *container) spec() (*specs.Spec, error) {
+	//	dt, err := ioutil.ReadFile(filepath.Join(ctr.dir, configFilename))
+	//	if err != nil {
+	//		return nil, err
+	//	}
+	//	if err := json.Unmarshal(dt, &spec); err != nil {
+	//		return nil, err
+	//	}
+	//	return &spec, nil
+	//}
+
+	log.Printf("%s mount complete", pfile)
+	return
 }
